7bef8fcc2c40d5edd69dac16c3fda3945825c027
CallumHemsley <bythepenguin@gmail.com>
2018-06-18 09:28:43 +0100

    Now places order for entry price before the loop.

--- src/lib/runtime/runtimeoperations.py
+++ src/lib/runtime/runtimeoperations.py
@@ -5,6 +5,15 @@ class RuntimeOperations:
     def initiate(self, CyrCraft, Config, Exchange):
         currentOrder = ActiveOrder(Config.getCurrency(),
                                    Config.getSide())
+        currentOrder.add(Config.getAmount(), Config.getEntryPrice())
+        # place order!
+        order = Exchange.createOrder(Config.getCurrency(), Config.getSide(), currentOrder.getPrice(), Config.getAmount())
+        print("buy/sell response:\n" + str(order))
+        if order is None:
+            return False
+        if 'orderOid' in order:
+            # set the clientOrderId
+            currentOrder.setId(order['orderOid'])
         # lets start the runtime loop
         while CyrCraft.getState() != 'Standby':
             # lets determine which strategy to use
